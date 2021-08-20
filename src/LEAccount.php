b8a7ad098b5f0502fa5fe3b952a271b17e890ff8
James Clark <james@weno.net>
2021-08-20 14:02:22 -0500

    handle missing contact

--- src/LEAccount.php
+++ src/LEAccount.php
@@ -133,7 +133,7 @@ class LEAccount
 		{
 			$this->id = isset($post['body']['id']) ? $post['body']['id'] : '';
 			$this->key = $post['body']['key'];
-			$this->contact = $post['body']['contact'];
+			$this->contact = isset($post['body']['contact']) ? $post['body']['contact'] : '';
 			$this->agreement = isset($post['body']['agreement']) ? $post['body']['agreement'] : '';
 			$this->initialIp = $post['body']['initialIp'];
 			$this->createdAt = $post['body']['createdAt'];
